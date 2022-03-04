50461c6cfccfa486fc42724baf5b2ad2b5911af4
djgnfj-svg <djgnfj8923@naver.com>
2022-03-04 16:47:49 +0900

    fix url, home.html, settings

--- we_need/urls.py
+++ we_need/urls.py
@@ -13,18 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+
 from needs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sign in/', views.as_view(),name="sign in"),
-    path('sign up/', views.as_view(),name="sign up"),
-    path('profile/', views.as_view(),name="profile"),
-    path('prpfile/update', views.as_view(),name="profile-update"),
-    path('needs/', views.as_view(),name="needs-all"),
-    path('needs/<int:need_id>/', views.as_view(),name="needs-view"),
-    path('needs/<int:need_id>/create/', views.as_view(),name="needs-create"),
-    path('needs/<int:need_id>/update/', views.as_view(),name="needs-update"),
-]
+    path('', views.home_view, name="home"),
+    # path('sign in/', views.as_view(),name="sign in"),
+    # path('sign up/', views.as_view(),name="sign up"),
+    # path('profile/', views.as_view(),name="profile"),
+    # path('prpfile/update', views.as_view(),name="profile-update"),
+    # path('needs/', views.as_view(),name="needs-all"),
+    # path('needs/<int:need_id>/', views.as_view(),name="needs-view"),
+    # path('needs/<int:need_id>/create/', views.as_view(),name="needs-create"),
+    # path('needs/<int:need_id>/update/', views.as_view(),name="needs-update"),
+] 
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
