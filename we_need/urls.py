2cfed2851667f8daf77035cc3a70e8099333a9e0
djgnfj-svg <djgnfj8923@naver.com>
2022-03-04 16:00:57 +0900

    setting base.html, urls

--- we_need/urls.py
+++ we_need/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from needs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sign in/', views.as_view(),name="sign in"),
+    path('sign up/', views.as_view(),name="sign up"),
+    path('profile/', views.as_view(),name="profile"),
+    path('prpfile/update', views.as_view(),name="profile-update"),
+    path('needs/', views.as_view(),name="needs-all"),
+    path('needs/<int:need_id>/', views.as_view(),name="needs-view"),
+    path('needs/<int:need_id>/create/', views.as_view(),name="needs-create"),
+    path('needs/<int:need_id>/update/', views.as_view(),name="needs-update"),
 ]
