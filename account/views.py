6209109d5d9151536e675dfbc43cb8cd8f013c44
djgnfj-svg <djgnfj8923@naver.com>
2022-03-08 23:12:13 +0900

    add temp_tob_bar

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView, FormView, CreateView, ListView, DetailView, UpdateView, DeleteView
 from account.models import MyUser
+
+from django.contrib.auth import login, logout
 from . import form
 
 # Create your views here.
@@ -10,3 +12,11 @@ class SigninView(CreateView):
     template_name = 'pages/task_create.html'
     form_class = form.LoginForm()
     success_url = '/'
+    
+    def login(request, _user):
+        login(request, _user)
+        return redirect("home")
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
